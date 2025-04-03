f3f6c654db142b8c02b0a11255485963a0afb21d
Rotem Meron <rm13rotem@gmail.com>
2025-04-03 21:33:33 +0300

    boilerplate creation of an MVC.net core application

---
+++ CrfDesign.Server/CrfDesign.Server.WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrfDesign.Server.WebAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
