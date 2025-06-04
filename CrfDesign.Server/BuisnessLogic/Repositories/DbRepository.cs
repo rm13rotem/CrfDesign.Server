75f075e366e1b279f0ed8adadff1f2fbfa5f49ed
Rotem Meron <rm13rotem@gmail.com>
2025-06-04 12:56:25 +0300

    preparing repositories and InMemoryRepositories (prototype)

---
+++ CrfDesign.Server/BuisnessLogic/Repositories/DbRepository.cs
@@ -0,0 +1,83 @@
+﻿using BuisnessLogic.DataContext;
+using BuisnessLogic.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnessLogic.Repositories
+{
+    public class DbRepository<T> : IRepository<T>, IDbRepository<T> where T : class
+    {
+        internal CrfDesignContext context;
+        internal DbSet<T> _dbSet;
+
+        public DbRepository(CrfDesignContext context)
+        {
+            this.context = context;
+            this._dbSet = context.Set<T>();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<List<T>> GetAllAsync(bool tracked = true)
+        {
+            IQueryable<T> query = _dbSet.AsQueryable();
+
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<bool> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> DeleteByIdAsync(int id)
+        {
+            var entityToDelete = await _dbSet.FindAsync(id);
+
+            if (entityToDelete != null)
+            {
+                if (entityToDelete is IPersistantEntity dbEntity)
+                {
+                    dbEntity.IsDeleted = true;
+                    return await SaveAsync();
+                }
+            }
+            return false;
+        }
+
+
+
+        public async Task<bool> UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            try
+            {
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CrfDesign.Server/BuisnessLogic/Repositories/IDbRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BuisnessLogic.Repositories
+{
+    public interface IDbRepository<T> where T : class
+    {
+        Task<bool> AddAsync(T entity);
+        Task<bool> DeleteByIdAsync(int id);
+        Task<List<T>> GetAllAsync(bool tracked = true);
+        Task<T> GetByIdAsync(int id);
+        Task<bool> SaveAsync();
+        Task<bool> UpdateAsync(T entity);
+    }
+}
